1981a85f393dcd5dfd05aea32b3b9b7475a71ae0
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-03-24 15:18:53 +0100

    maj

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
 import { AppComponent } from './app/app.component';
 import { APP_CONFIG } from './app/app.config';
+
+DotLottie.setWasmUrl('/assets/wasm/dotlottie-player.wasm');
 
 try {
   await bootstrapApplication(AppComponent, APP_CONFIG);
