14c1dabc929646ed00e5d0ad6c22542630b1abf8
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-03-25 19:00:10 +0100

    test

---
+++ public/maintenance.js
@@ -0,0 +1,49 @@
+// --- I18n ---
+const translations = {
+  fr: {
+    title: 'Une erreur est survenue.',
+    label: 'Erreur',
+    subtitle: 'Casques obligatoires !',
+    message: 'On laisse sécher le code et on revient vite...',
+    ariaLabel: "Une équipe technique s'affairant autour d'un écran en maintenance"
+  },
+  en: {
+    title: 'An error has occurred.',
+    label: 'Error',
+    subtitle: 'Helmets are needed !',
+    message: "We'll let the code dry and we'll be back soon...",
+    ariaLabel: 'A technical team working on a screen undergoing maintenance'
+  }
+};
+
+const userLang = navigator.language || navigator.userLanguage;
+const lang = userLang.startsWith('en') ? 'en' : 'fr';
+
+document.getElementById('h1').innerText = translations[lang].title;
+document.getElementById('h3').innerText = translations[lang].label;
+document.getElementById('h4').innerText = translations[lang].subtitle;
+document.getElementById('h5').innerText = translations[lang].message;
+
+document.getElementById('lottie').setAttribute('aria-label', translations[lang].ariaLabel);
+
+if (lang === 'en') {
+  document.getElementById('h3-wrapper').style.flexDirection = 'row-reverse';
+}
+
+document.documentElement.lang = lang;
+
+// AUTO-REFRESH ---
+setInterval(async () => {
+  try {
+    const response = await fetch(window.location.href, {
+      method: 'HEAD',
+      cache: 'no-store'
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    }
+  } catch (error) {
+    console.info('Website still under maintenance...');
+  }
+}, 30000);
