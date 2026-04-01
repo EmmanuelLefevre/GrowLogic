f4cba3a979d13221a83b95413177cbd9124aeae4
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-04-01 13:18:49 +0200

    Add Supabase service tests

--- src/app/core/_services/supabase/supabase.service.spec.ts
+++ src/app/core/_services/supabase/supabase.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { SupabaseClient } from '@supabase/supabase-js';
 
-import { SupabaseService } from '../supabase.service';
+import { SupabaseService } from './supabase.service';
 
 describe('SupabaseService', () => {
+
   let service: SupabaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SupabaseService]
+    });
     service = TestBed.inject(SupabaseService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should expose the supabase client', () => {
+    const client: SupabaseClient = service.client;
+
+    expect(client).toBeDefined();
+    expect(client).toBeInstanceOf(Object);
   });
 });
